53e628812b4216df218cbd38b746d199e1c60579
swaranjit banerjee <swaranjitbanerjee7@gmail.com>
2025-01-25 20:35:07 +0530

    marker customized

--- public/js/script.js
+++ public/js/script.js
@@ -29,18 +29,26 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 // Keep track of markers for connected users
 const markers = {};
 
+// Define a custom marker icon
+const customIcon = L.icon({
+    iconUrl: "https://cdn-icons-png.flaticon.com/512/1077/1077012.png", // Replace with your custom icon URL
+    iconSize: [32, 32], // Size of the icon
+    iconAnchor: [16, 32], // Point of the icon that will correspond to the marker's location
+    popupAnchor: [0, -32], // Point where the popup should open relative to the icon
+});
+
 // Listen for location updates from the server
 socket.on("receive-location", (data) => {
     const { id, latitude, longitude } = data;
 
     // Set the map's view to the new location
-    map.setView([latitude, longitude],);
+    map.setView([latitude, longitude], 10);
 
     // Update marker for the user or create a new one
     if (markers[id]) {
         markers[id].setLatLng([latitude, longitude]);
     } else {
-        markers[id] = L.marker([latitude, longitude]).addTo(map);
+        markers[id] = L.marker([latitude, longitude], { icon: customIcon }).addTo(map);
     }
 });
 
